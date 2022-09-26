3c6b1e69761ea1b7ed8c7fbfb2030649b42b477e
Mouncef <elouazzanim@wit.edu>
2022-09-26 13:53:25 -0400

    oh he's a hershey man

--- GuessingGam/src/game/GuessingGame.java
+++ GuessingGam/src/game/GuessingGame.java
@@ -9,10 +9,36 @@ public class GuessingGame {
 		user = new LinkedBag<Integer>();
 		game = new LinkedBag <Integer>();
 		System.out.println("In this game you select a number of integers to guess "
-				+ "The system generates this number of random integers. "
+				+ "The system generates this number of random integers (2 to whatever you select). "
 				+ "They are not necessarily distinct "
 				+ "You will try to guess them in several attempts.");
 	}
 	
+	private void initializeGame(int size, int range) {
+		for(int i = 0; i < size; i++) {
+			int a = (int) ((Math.random() * (range - 1)) + 1);
+			game.add(a);
+		}
+	}
+	
+	private int getSize() {
+		return game.getCurrentSize();
+	}
+	
+	private void addFromUser(Integer value) {
+		user.add(value);
+	}
+	
+	private int numIntersections() {
+		return game.intersection(user).getCurrentSize();
+	}
+	
+	private void clearUser() {
+		user.clear();
+	}
+	
+	public static void play() {
+		
+	}
 	
 }
